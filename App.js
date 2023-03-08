005ea7bc05a91c033e60444c8c451e509688a3a9
it-akademyAxLDvL <a.denis@it-students.fr>
2023-03-08 16:50:10 +0100

    tab

--- App.js
+++ App.js
@@ -6,41 +6,21 @@ import HomeScreen from "./view/Home";
 import DetailsScreen from "./view/Details";
 import FavoritesScreen from "./view/Favorites"
 import Ionicons from "@expo/vector-icons/Ionicons"
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {StatusBar} from "react-native";
 
 
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator
 
 export default function App() {
     return (
-
-        <NavigationContainer>
-
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-
-                        if (route.name === 'Home') {
-                            iconName = focused
-                                ? 'home' : 'home-outline';
-                        } else if (route.name === 'Details') {
-                            iconName = focused ? 'ios-list' : 'ios-list-outline';
-                        } else if (route.name === 'Favorites') {
-                            iconName = focused ? 'md-heart-sharp' : 'md-heart-outline';
-                        }
-
-                        // You can return any component that you like here!
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                    tabBarActiveTintColor: 'tomato',
-                    tabBarInactiveTintColor: 'gray',
-                })}
-            >
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Details" component={DetailsScreen} />
-                <Tab.Screen name="Favorites" component={FavoritesScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
+    <NavigationContainer>
+        <StatusBar style="auto"/>
+        <Stack.Navigator>
+            <Stack.Screen name={"Home"} component ={HomeScreen}/>
+            <Stack.Screen name={"Details"} component ={DetailsScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
 
     );
 }
--- view/Home.js
+++ view/Home.js
@@ -1,14 +1,22 @@
 import * as React from 'react';
-import {Button, View, Text, TouchableOpacity, StatusBar, StyleSheet, SafeAreaView, FlatList} from 'react-native';
+import {Button, View, Text, TouchableOpacity, StatusBar, StyleSheet, SafeAreaView, FlatList, Image} from 'react-native';
 import {useEffect, useState} from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import DetailsScreen from "./Details";
+import FavoritesScreen from "./Favorites";
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
 
 const Item = ({item, onPress, backgroundColor, textColor}) => (
     <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={[styles.title, {color: textColor}]}>{item.strDrink}</Text>
+        </View>
+        <Image style={{width:150,height:150}} source={{uri:item.strDrinkThumb}} />
     </TouchableOpacity>
 );
-
+const Tab = createBottomTabNavigator();
 export default function HomeScreen({ navigation }) {
     const [coktailList, setCoktailList] = useState([])
     const [selectedId, setSelectedId] = useState();
@@ -28,8 +36,8 @@ export default function HomeScreen({ navigation }) {
     },[])
 
     const renderItem = ({item}) => {
-        const backgroundColor = item.idDrink === selectedId ? '#6e3b6e' : '#f9c2ff';
-        const color = item.idDrink === selectedId ? 'white' : 'black';
+        const backgroundColor = item.idDrink === selectedId ? 'white' : 'white';
+        const color = item.idDrink === selectedId ? 'grey' : 'black';
 
         return (
             <Item
@@ -47,6 +55,33 @@ export default function HomeScreen({ navigation }) {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
+
+                            if (route.name === 'Home') {
+                                iconName = focused
+                                    ? 'home' : 'home-outline';
+                            } else if (route.name === 'Details') {
+                                iconName = focused ? 'ios-list' : 'ios-list-outline';
+                            } else if (route.name === 'Favorites') {
+                                iconName = focused ? 'md-heart-sharp' : 'md-heart-outline';
+                            }
+
+                            // You can return any component that you like here!
+                            return <Ionicons name={iconName} size={size} color={color} />;
+                        },
+                        tabBarActiveTintColor: 'tomato',
+                        tabBarInactiveTintColor: 'gray',
+                    })}
+                >
+                    <Tab.Screen name="Home" component={HomeScreen} />
+                    <Tab.Screen name="Details" component={DetailsScreen} options={{headerShown: false}} />
+                    <Tab.Screen name="Favorites" component={FavoritesScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
             <Text>Home Screen</Text>
             <Button
                 title="Go to Details"
@@ -67,14 +102,23 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection:"column",
         marginTop: StatusBar.currentHeight || 0,
     },
     item: {
+        flex:1,
+        width:150,
+        height:150,
+        alignItems: "center",
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
+        shadowColor: '#171717',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
     },
     title: {
-        fontSize: 32,
+        fontSize: 14,
     },
 });
