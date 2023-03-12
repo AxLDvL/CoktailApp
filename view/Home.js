38a8f2d4f11ac109851ce0d227ecb1156c0f2955
AxLDvL <a.denis@it-students.fr>
2023-03-12 21:43:36 +0100

    searchBar without logic

--- view/Home.js
+++ view/Home.js
@@ -4,7 +4,7 @@ import {
     StatusBar,
     StyleSheet,
     SafeAreaView,
-    FlatList, TouchableOpacity,
+    FlatList, TouchableOpacity
 
 } from 'react-native';
 import { useEffect, useState} from "react";
@@ -12,10 +12,13 @@ import ShowCoktailImage from "../components/ShowCoktailImage";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import {useDispatch, useSelector} from "react-redux";
 import {createFavorite, deleteFavorite} from "../redux";
+import SearchBar from "../components/SearchBar";
 
 const HomeScreen = ({ navigation }) => {
     const [coktailList, setCoktailList] = useState([])
     const [selectedId, setSelectedId] = useState("");
+    const [itemsPerPage, setItemsPerPage] = useState(20);
+    const [searchPhrase, setSearchPhrase] = useState('')
 
     const favorites = useSelector((state) => state.favoriteCok);
 
@@ -23,36 +26,41 @@ const HomeScreen = ({ navigation }) => {
 
     useEffect(()=>{
         try{
-            const coktailList = (async () => {
-                const response = await fetch ('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink');
-                const datas = await response.json();
-                console.log(datas.drinks)
-                setCoktailList(datas.drinks);
-            })();
+            fetch ('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink')
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.drinks)
+                    setCoktailList(data.drinks);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
         }catch(e){
             console.error(e)
         }
         console.log(coktailList);
     },[])
 
+
+
     const isFavorite = (item) => {
         return favorites.some(e => e.idDrink === item.idDrink);
     }
-    const renderItem = ({item}) => {
+    const renderItem = ({item, index}) => {
         const backgroundColor = item.idDrink === selectedId ? 'white' : 'white';
         const color = item.idDrink === selectedId ? 'darkgrey' : 'grey';
-
+    if(index < itemsPerPage) {
         return (
             <View style={styles.contain}>
-            <ShowCoktailImage
-                item={item}
-                onPress={() => {
-                    setSelectedId(item.idDrink);
-                    navigation.navigate('Details',{idDrink:item.idDrink})
-                }}
-                backgroundColor={backgroundColor}
-                textColor={color}
-            />
+                <ShowCoktailImage
+                    item={item}
+                    onPress={() => {
+                        setSelectedId(item.idDrink);
+                        navigation.navigate('Details',{idDrink:item.idDrink})
+                    }}
+                    backgroundColor={backgroundColor}
+                    textColor={color}
+                />
                 <View style={{flexDirection:"row-reverse"}}>
                     <TouchableOpacity
                         onPress={() => {
@@ -64,29 +72,36 @@ const HomeScreen = ({ navigation }) => {
                             } else{
                                 dispatch(deleteFavorite(item.idDrink))
                             }
-                            }}
-                        style={[styles.item, {backgroundColor:'transparent'}]}
+                        }}
+                        style={[styles.item, {backgroundColor:'transparent',marginHorizontal:10,marginBottom:10}]}
                     >
-                            {isFavorite(item)?
+                        {isFavorite(item)?
                             <Ionicons  name="ios-heart" size={24} color="red" />
-                                :
+                            :
                             <Ionicons  name="ios-heart-outline" size={24} color="red" />
-                            }
+                        }
                     </TouchableOpacity>
                 </View>
             </View>
-        );
+        )}
     };
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <SearchBar
+                searchPhrase={searchPhrase}
+                setSearchPhrase={setSearchPhrase}/>
             <SafeAreaView style={styles.container}>
                     <FlatList
-                        data={coktailList}
+                        data={coktailList.slice(0, itemsPerPage)}
                         renderItem={renderItem}
                         keyExtractor={item => item.idDrink}
                         extraData={[selectedId, favorites]}
                         numColumns={2}
+                        onEndReached={() => {
+                            setItemsPerPage(itemsPerPage + 20);
+                        }}
+                        onEndReachedThreshold={5}
                     />
             </SafeAreaView>
         </View>
@@ -100,11 +115,15 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
         width:"100%",
-        height:"100%"
+        height:"100%",
+
     },
     contain:{
         flex:1,
-        flexDirection:"column"
+        flexDirection:"column",
+        backgroundColor:"white",
+        margin:10,
+        borderRadius:10
     }
 });
 
